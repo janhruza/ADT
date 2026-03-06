918d9064ee9845899ab87613f0f3cd4ec208bb49
Endeavour-PC <honza.hruza.d5@seznam.cz>
2026-03-06 10:51:51 +0100

    Progress.

---
+++ Lessons/3/main.py
@@ -0,0 +1,126 @@
+from collections import defaultdict
+import os
+import sys
+from dataclasses import dataclass
+
+@dataclass
+class Record:
+    time: int
+    id_cust: int
+
+def load_data(data_path: str, city: str, shop: str, day: str = "1-Mon") -> \
+        dict[str, list[Record]] | None:
+    """ Funkce načte data z daného souboru a vrátí je jako slovník.
+    Klíčem je název checkpointu a hodnotou je list záznamů.
+
+    Args:
+        data_path (str): cesta k adresáři se všemi daty
+        city (str): název města, které chceme načíst
+        shop (str): název obchodu, který chceme načíst
+        day (str, optional): Konkrétní den, který chceme načíst. Defaults to "1-Mon".
+
+    Returns:
+        dict[str, list[Record]] | None: slovník s načtenými daty nebo None pokud soubor neexistuje
+    """
+
+    # pozn. Můžeme použít default dict, nebo použít běžný slovník a při přidání nového záznamu
+    # vždy zkontrolovat, zda klíč již existuje, případně inicializovat prázdný list
+
+    city_data: dict[str, list[Record]] = {}
+    # city_data: dict[str, list[Record]] = defaultdict(list)
+
+    print("loading", city)
+    
+    # muze existovat vice suboru v cilovem adresari
+    sDir = data_path + os.path.sep + city + os.path.sep + day
+
+    if not os.path.exists(sDir):
+        return None
+
+    sFile = sDir + os.path.sep + shop + ".txt"
+    if os.path.exists(sFile):
+        with open(sFile, "r") as f:
+            lines = f.readlines()
+            for i in range(1, len(lines)):
+                    line = lines[i].strip()
+                    if line:
+                        time, dum1, id_cust, dum2 = line.split(';')
+                        if time not in city_data:
+                            city_data[time] = []
+                        city_data[time].append(Record(int(time), int(id_cust)))
+
+    return city_data
+
+def get_passed_set(data: dict[str, list[Record]], key_words: list[str]) -> set[int]:
+    """Funkce vrátí množinu zákazníků, kteří prošli alespoň jedním z checkpointů s prefixem
+    předaných jako key_words. Do funkce tedy nevstupuje celé jméno checkpointu ale pouze
+    jeho prefix (např. vege místo vege_1).
+
+    Args:
+        data (dict[str, list[Record]]): data načtená z datového souboru funkcí load_data
+        key_words (list[str]): prefixové označení checkpointů, které chceme sledovat
+
+    Returns:
+        set[int]: Funkce vrací množinu identifikačních čísel zákazníků.
+    """
+    customers: set[int] = set()
+
+    for ckpt, records in data.items():
+        for kw in key_words:
+            if ckpt.startswith(kw):
+                for record in records:
+                    customers.add(record.id_cust)
+                break
+
+    return customers
+
+def filter_data_time(data: dict[str, list[Record]], cond_time: int) -> dict[str, list[Record]]:
+    """Funkce vrátí data omezená na záznamy s časem menším nebo rovným než je cond_time.
+    Args:
+        data (dict[str, list[Record]]): data načtená z datového souboru funkcí load_data
+        cond_time (int): časový limit v sekundách
+    Returns:
+        dict[str, list[Record]]: vrací data omezená na záznamy s časem menším nebo rovným cond_time.
+    """
+    ret: dict[str, list[Record]] = defaultdict(list)
+
+    for ckpt, records in data.items():
+        for record in records:
+            if record.time <= cond_time:
+                ret[ckpt].append(record)
+
+    return ret
+
+def get_q_size(data: dict[str, list[Record]], seconds: int) -> int:
+    """Funkce vrátí velikost fronty v daném čase.
+    Velikost fronty je dána počtem zákazníků, kteří prošli některým z checkpointů
+    (vege, frui, meat) a ještě neprošli pokladnou.
+    """
+
+    return 0
+
+def histogram(data: dict[str, list[Record]]) -> None:
+    for time in data:
+        print(time, get_q_size(data, time))
+
+def main(data_path: str) -> None:
+    while True:
+        city = input("Zadejte město (Plzeň): ")
+        shop = input("Zadejte obchod (shop_a): ")
+
+        if city == "":
+            city = "Plzeň"
+        if shop == "":
+            shop = "shop_a"
+
+        data = load_data(data_path, city, shop)
+        if data is None:
+            continue
+
+        histogram(data)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <data_path>")
+        sys.exit(1)
+    main(sys.argv[1])
